9e6a4612076a5bf2b41188041d225b764659135e
Vinura <vinuraran.16@cse.mrt.ac.lk>
2023-08-26 02:04:12 +0530

    create expense tracker api and integrate with mongo db and mongock

---
+++ src/main/java/com/microservices/productservice/controller/ExpenseController.java
@@ -0,0 +1,47 @@
+package com.microservices.productservice.controller;
+
+//import com.programming.techie.mongo.service.ExpenseService;
+import com.microservices.productservice.model.Expense;
+import com.microservices.productservice.service.ExpenseService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/expense")
+// @RequiredArgsConstructor
+public class ExpenseController {
+    private final ExpenseService expenseService;
+
+    public ExpenseController(ExpenseService expenseService) {
+        this.expenseService = expenseService;
+    }
+
+    @PostMapping
+    public ResponseEntity addExpense(@RequestBody Expense expense){
+        expenseService.addExpense(expense);
+        return  ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+    @PutMapping
+    public ResponseEntity updateExpense(@RequestBody Expense expense){
+        expenseService.updateExpense(expense);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+    @GetMapping
+    public ResponseEntity<List<Expense>> getAllExpenses(){
+        return ResponseEntity.ok(expenseService.getAllExpenses());
+    }
+    @GetMapping("/{name}")
+    public ResponseEntity<Expense> getExpenseByName(@PathVariable String name) {
+        return ResponseEntity.ok(expenseService.getExpenseByName(name));
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteExpense(@PathVariable String id) {
+        expenseService.deleteExpense(id);
+        return ResponseEntity.noContent().build();
+    }
+    //private final ExpenseService expenseService;
+
+}
